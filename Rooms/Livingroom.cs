5b4830cf94e3a685450e4cca9551b55373a896a6
kim pahud <Kphd64@gmail.com>
2023-11-03 01:36:47 -0400

    add livingroom final

--- Rooms/Livingroom.cs
+++ Rooms/Livingroom.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace ProjetNarratif.Rooms
 {
-    internal class Livingroom
+    internal class Livingroom : Room
     {
+        internal override string CreateDescription() =>
+@"Une fois la porte du salon ouvert,dans le salon
+il y a un [sofa] et un [tv].
+Tu peux revenir dans ta [chambre].
+";
+
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+
+                case "sofa":
+                    Console.WriteLine("tu commences à dormir");
+                    Game.Transition<Livingroom>();
+                    break;
+                case "tv":
+                    Console.WriteLine("Tu t'approches de la télévision, et tu commences à toucher la tv, et un vortex interdimensionnel t'aspires dans un autre monde.");
+                    Game.Finish();
+                    break;
+                case "chambre":
+                    Console.WriteLine("Tu retournes dans ta chambre.");
+                    Game.Transition<Bedroom>();
+                    break;
+                default:
+                    Console.WriteLine("Commande invalide.");
+                    break;
+            }
+        }
     }
 }
