1be59205a2b6f711e6518fd7eb60b59d101f8f1b
CuteChair <jules.lhp@gmail.com>
2023-11-22 11:07:08 -0500

    choix outsideRoom

--- Rooms/OutsideRoom.cs
+++ Rooms/OutsideRoom.cs
@@ -9,23 +9,41 @@ namespace ProjetNarratif.Rooms
     internal class OutsideRoom : Room
     {
         internal override string CreateDescription() =>
-@"Dans le Salon, Malgré que l'atmosphère soie chaleureuse, quelque chose cloche...
-Une télévision grésille [television].
-S'assoir sur le sofa [sofa].
+@"Tu es sur le patio de ta maison.
+Lorsque tu regardes vers l'horizon, tu ne vois presque rien.
+Un épais brouillard bloque ton champs de vision
+À tes pieds tu trouves un tapis de bienvenu [tapis]
+À ta gauche se trouve une boite au lettre [boite]
+Malgré le brouillard, tu arrives à percevoir un arbre au loin. [arbre]
+Tu décides de rentrer à l'intérieur [rentre] 
 ";
-
+        static bool backdoorkey = false;
         internal override void ReceiveChoice(string choice)
         {
             switch (choice)
             {
-                case "television":
-                    Console.WriteLine("Une fois devant la télévision le grésillement arrête. Tu te sens hypnotiser par ce que tu vois puis... Rien...");
-                    Game.Finish();
+                case "tapis":
+                    Console.WriteLine("Tu regardes en dessous du tapis, mais tu ne trouves rien");
+                    
                     break;
-                case "sofa":
+                case "boite":
+                    Console.WriteLine("Tu regardes dans la boite au lettre, elle est vide...");
+
+                    break;
+                case "arbre":
+                    Console.WriteLine("Une fois devant la télévision le grésillement arrête. Tu te sens hypnotiser par ce que tu vois puis... Rien...");
+
+                    break;
+                case "rentre":
                     Console.WriteLine("Le bruis de la télévision s'intensifie, tu n'entends que ça...");
 
                     break;
+                case "clef":
+                    Console.WriteLine("Tu plonge ta mains dans la boite au lettre et tâtes les rebords du bout des doigts." +
+                        "\nTu trouves la clef que ton père avait caché");
+                             backdoorkey = true;
+                    break;
+
                 default:
                     Console.WriteLine("Commande invalide.");
                     break;
